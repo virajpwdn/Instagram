c5cfe1afcd6c58354e8eaef1b15fd1d61c634c77
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-13 10:40:49 +0530

    feature: chat && implemented socket io && added middleware to verify

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import app from './src/app.js';
 import http from 'http';
 import config from './src/config/config.js';
 import connectDB from './src/database/db.js';
+import initSocket from './src/sockets/socket.io.js';
 
 const server = http.createServer(app);
 connectDB();
+initSocket(server);
 
 server.listen(config.PORT, ()=>{
     console.log("Server is connected");
--- src/sockets/socket.io.js
+++ src/sockets/socket.io.js
@@ -1,1 +1,33 @@
-import { Socket } from "socket.io";
+import { Server } from "socket.io";
+import userModel from "../models/user.js";
+
+function initSocket(server) {
+  console.log("Initialised socket io");
+  const io = new Server(server);
+
+  io.use(async (socket, next) => {
+    try {
+      const token = socket.handshake.headers.token;
+      if (!token) return next(new Error("Token not found!"));
+
+      const decodeToken = userModel.verifyToken(token);
+      if (!decodeToken) return next(new Error(decodeToken));
+
+      const user = await userModel.findOne({ _id: decodeToken._id });
+      if (!user) return next(new Error("user not found"));
+
+      socket.user = user;
+      console.log(socket.user);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  io.on("connection", async (socket) => {
+    console.log("user is connected with socket");
+  });
+}
+
+export default initSocket;
