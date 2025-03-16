e3dd7fe038afd02a060ed0c9586c218703130ca6
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-16 19:49:37 +0530

    cors setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,22 @@
-import app from './src/app.js';
-import http from 'http';
-import config from './src/config/config.js';
-import connectDB from './src/database/db.js';
-import initSocket from './src/sockets/socket.io.js';
+import app from "./src/app.js";
+import http from "http";
+import config from "./src/config/config.js";
+import connectDB from "./src/database/db.js";
+import initSocket from "./src/sockets/socket.io.js";
+import cors from "cors";
 
 const server = http.createServer(app);
 connectDB();
 initSocket(server);
 
-server.listen(config.PORT, ()=>{
-    console.log("Server is connected");
-})
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+server.listen(config.PORT, () => {
+  console.log("Server is connected");
+});
