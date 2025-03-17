0dd468cf660fec3c6009a28961622b114b344128
virajpwdn <virajpatwardhan6@gmail.com>
2025-03-17 07:31:22 +0530

    cors moved to app from server

--- Backend/server.js
+++ Backend/server.js
@@ -3,18 +3,12 @@ import http from "http";
 import config from "./src/config/config.js";
 import connectDB from "./src/database/db.js";
 import initSocket from "./src/sockets/socket.io.js";
-import cors from "cors";
 
 const server = http.createServer(app);
 connectDB();
 initSocket(server);
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    allowedHeaders: ["Content-Type", "Authorization"],
-  })
-);
+
 
 server.listen(config.PORT, () => {
   console.log("Server is connected");
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,9 +4,19 @@ import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes.js";
 import aiRouter from "./routes/ai.routes.js";
 import postRouter from "./routes/post.routes.js";
+import cors from "cors";
 
 
 const app = express();
+
+app.use(
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true, // if using cookies or tokens
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
 
 app.use(morgan("dev"));
 app.use(express.json());
